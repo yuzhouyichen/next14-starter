fef41e55455e464411c7d9bf4b22c069b17f1d7c
wangqiang <zhulinlin@wintelia.com>
2024-06-05 14:04:36 +0800

    about page finish

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,28 @@ import styles from "./about.module.css";
 
 const AboutPage=()=>{
     return (
-        <div>
+        <div className={styles.container}>
+            <div className={styles.textContainer}>
+                <h2 className={styles.subtitle}>About Agency</h2>
+                <h1 className={styles.title}>We create digital ideas that are bigger, bolder, braver and better.</h1>
+                <p className={styles.desc}>We create digital ideas that are bigger, bolder, braver and better. We believe in good ideas flexibility and precission We’re world’s Our Special Team best consulting & finance solution provider. Wide range of web and software development services.</p>
+                <div className={styles.itemContainer}>
+                    <div className={styles.item}>
+                        <h2>10 K+</h2>
+                        <p>Year of experience</p>
+                    </div>
+                    <div className={styles.item}>
+                        <h2>10 K+</h2>
+                        <p>Year of experience</p>
+                    </div>
+                    <div className={styles.item}>
+                        <h2>10 K+</h2>
+                        <p>Year of experience</p>
+                    </div>
+                </div>
+            </div>
             <div className={styles.imgContainer}>
-                <Image src="/about.png" alt="about image" fill/>
+                <Image className={styles.img} src="/about.png" alt="about image" fill/>
             </div>
         </div>
     );
