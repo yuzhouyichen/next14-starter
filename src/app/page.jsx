bd106e53a7fa2aa1df08bd97dd0aefdeda0635ec
wangqiang <zhulinlin@wintelia.com>
2024-06-05 11:53:59 +0800

    finish home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,26 @@
+import styles from "./home.module.css"
+import Image from "next/image";
+
 const Home = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div className={styles.container}>
+    <div className={styles.textContainer}>
+      <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+      <p className={styles.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis adipisci minima reiciendis a autem assumenda dolore.</p>
+      <div className={styles.buttonContainer}>
+        <button className={styles.button}>Learn more</button>
+        <button className={styles.button}>Contact</button>
+      </div>
+      <div className={styles.brandContainer}>
+        <Image src="/brands.png" alt="brands" fill className={styles.brandImg}/>
+      </div>
+    </div>
+    <div className={styles.imageContainer}>
+      <Image src="/hero.gif" alt="hero" fill className={styles.heroImg}/>
+    </div>
+  </div>
+  );
+
 };
 
 export default Home;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,16 @@
+import Image from "next/image";
+import styles from "./footer.module.css"
+
 const Footer=()=>{
     return (
-        <div>Footer</div>
+        <div className={styles.container}>
+            <div className={styles.logo}>
+                logo
+            </div>
+            <div className={styles.text}>
+                <p>Lama creative thoughts agency © All rights reserved.</p>
+            </div>
+        </div>
     );
 }
 export default Footer;
