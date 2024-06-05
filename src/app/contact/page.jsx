62727622c3e56c8d91f38d98985a20c578d8f2c8
wangqiang <zhulinlin@wintelia.com>
2024-06-05 14:35:13 +0800

    finish contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,21 @@
+import Image from "next/image";
+import styles from "./contact.module.css"
 const ContactPage=()=>{
     return (
-        <div>ContactPage</div>
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image className={styles.img} src="/contact.png" alt="contact" fill />
+            </div>
+            <div className={styles.formContainer}>
+                <form className={styles.form}>
+                    <input type="text" placeholder="name and surname"/>
+                    <input type="email" placeholder="Email address" />
+                    <input type="text" placeholder="Phone Number(Optional)" />
+                    <textarea cols="30" rows="10" placeholder="Message"/>
+                    <button className={styles.send}>Send</button>
+                </form>
+            </div>
+        </div>
     );
 }
 
